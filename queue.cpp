1e25fe33bbaf28ca5ab27380c7a22f2585cb0cf8
AARYAN <aaryansingh.2905@gmail.com>
2023-07-31 14:07:52 +0530

    Add files via upload

---
+++ queue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    queue<string> q;
+    q.push("aaryan");
+    q.push("mohit");
+    q.push("dona dahiya");
+    // cout<<q.front();
+    // cout<<q.back();
+    // q.pop();
+    cout<<q.size();
+
+    return 0;
+}
